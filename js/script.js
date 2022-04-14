f87149c30c41f48133289fa0b76853bbc8f5cc4e
Natalia Szlapka <n.machalica@gmail.com>
2022-04-14 10:35:53 +0200

    Add script to page - change img and color after clicking thumb at the bottom of the page

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function imgSlider(starbucks) {
+	document.querySelector('.matcha').src = starbucks;
+}
+
+function circleColorChange(color) {
+	const circle = document.querySelector('.circle');
+	circle.style.background = color;
+}
