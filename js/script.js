7e823dcc500123032bf28a7bb160680b27d77fe8
Natalia Szlapka <n.machalica@gmail.com>
2022-04-14 13:14:26 +0200

    Add toggling menu to smaller resolutions and style it

--- js/script.js
+++ js/script.js
@@ -6,3 +6,10 @@ function circleColorChange(color) {
 	const circle = document.querySelector('.circle');
 	circle.style.background = color;
 }
+
+function toggleMenu(){
+	const menuToggle = document.querySelector('.toggle');
+	const navigation = document.querySelector('.navigation');
+	menuToggle.classList.toggle('active');
+	navigation.classList.toggle('active');
+}
